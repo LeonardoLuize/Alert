1da3ad17f559b073faf948815ef5445a4bb63fdc
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-07-26 15:15:57 -0300

    new get and post methods, for list and add features

--- index.js
+++ index.js
@@ -4,8 +4,27 @@ const app = express();
 const path = require('path');
 const PORT = 3000
 
+var allNotes = [{title: "oi", description: "dd"}];
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(express.json());
+
+app.post('/api/messages/add', (req, res) => {
+    var title = req.body.title
+    var description = req.body.description
+
+    var message = {title, description}
+
+    allNotes.push(message);
+
+    res.sendStatus(200)
+    
+})
+
+app.get('/api/messages/all', (req, res) => {
+    res.send(allNotes);
+})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
