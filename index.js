917c3ceff6f2346c1d30678649c10b45497ad8fd
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-07-26 12:09:04 -0300

    linked the index page

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const path = require('path');
 const PORT = 3000
 
 
+app.use(express.static(path.join(__dirname, '/public')))
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
