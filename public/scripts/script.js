e60388add3b098b222f68e44fdf03507cb7935fa
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-08-02 15:02:04 -0300

    now is receving the data from inputs, sending and loading the data

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,12 @@
+document.querySelector('body').addEventListener('mousemove', (event) => {
+    mouseX = event.clientX;
+    mouseY = event.clientY;
+})
+
+window.addEventListener('load', () => {
+    loadMessages()
+})
+
 function loadMessages(){
 
     fetch('http://localhost:3000/api/messages/all')
@@ -5,17 +14,43 @@ function loadMessages(){
         console.log(res);
 
         res.json().then(data => {
+
+            messageContainer = document.getElementById('wrapMessage');
             console.log(data);
+
+            messageContainer.innerHTML = ''
+
+            data.forEach(message => {
+
+                messageContainer.innerHTML += 
+                                                `
+                                                <div class="messageContainer">
+                                                    <div class="noteHeader">
+                                                        <h2>${message.title}</h2>
+
+                                                        <img src="./svg/delete.svg" alt="Deletar Mensagem">
+                                                    </div>
+
+                                                    <p>
+                                                        ${message.description}
+                                                    </p>
+                                                </div>
+                                                `
+
+            })
+           
         })
     })
     .catch(err => console.log('Novo erro: ' + err));
 }
 
 function addMessage(){
-    title = 'oo esse é o título novo'
-    description = 'oo essa é a descrição nova'
+    title = document.getElementById('messageTitle').value;
+    description = document.getElementById('messageDescription').value;
 
     newMessage = {title, description};
+
+  
 
     fetch('http://localhost:3000/api/messages/add', {
         method: 'POST',
@@ -24,7 +59,28 @@ function addMessage(){
     })
     .then(res => {
         console.log(res);
+        
+        toggleAddMessage()
         loadMessages();
     })
     .catch(err => console.log('Novo erro: ' + err))
+}
+
+async function toggleAddMessage(){
+    reset = resetInputs()
+
+    await reset
+
+    var modalBackground = document.querySelector('.modalBackground')
+
+    modalBackground.classList.toggle('modalOpen')
+
+}
+
+function resetInputs(){
+    title = document.getElementById('messageTitle').value;
+    description = document.getElementById('messageDescription').value;
+
+    title = " "
+    description = ""
 }
