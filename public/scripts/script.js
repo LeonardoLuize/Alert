1a8a7266026d34c5e753b6ef0ce70663626fcae8
Leonardo Luize <74014082+LeonardoLuize@users.noreply.github.com>
2021-08-03 11:52:20 -0300

    new search feature, made similar to add feature, the difference is that on search, is validated the input title and compared to all messages titles

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -28,7 +28,7 @@ function loadMessages(){
                                                     <div class="noteHeader">
                                                         <h2>${message.title}</h2>
 
-                                                        <img src="./svg/delete.svg" alt="Deletar Mensagem">
+                                                        <img onclick="deleteMessage('${message.id}')" src="./svg/delete.svg" alt="Deletar Mensagem">
                                                     </div>
 
                                                     <p>
@@ -41,14 +41,15 @@ function loadMessages(){
            
         })
     })
-    .catch(err => console.log('Novo erro: ' + err));
+    .catch(err => console.log('Novo erro no loadMessages: ' + err));
 }
 
 function addMessage(){
+    id = newId();
     title = document.getElementById('messageTitle').value;
     description = document.getElementById('messageDescription').value;
 
-    newMessage = {title, description};
+    newMessage = {id, title, description};
 
   
 
@@ -63,7 +64,63 @@ function addMessage(){
         toggleAddMessage()
         loadMessages();
     })
-    .catch(err => console.log('Novo erro: ' + err))
+    .catch(err => console.log('Novo erro no addMessage: ' + err))
+}
+
+
+function deleteMessage(id){
+    fetch(`http://localhost:3000/api/message/delete/${id}`, {method: 'DELETE'})
+    .then(res => {
+        console.log(res)
+        loadMessages();
+    })
+    .catch(err => console.log('Novo erro no deleteMessage: ' + err))
+}
+
+function searchMessage(){
+
+
+    fetch('http://localhost:3000/api/messages/all')
+    .then(res => {
+        console.log(res);
+
+        res.json().then(data => {
+
+            var messageTitle = document.getElementById('searchInput').value;
+            var messageContainer = document.getElementById('wrapMessage');
+            console.log(data);
+
+            messageContainer.innerHTML = ''
+
+            data.forEach(message => {
+
+                if(message.title == messageTitle){
+
+                    messageContainer.innerHTML += 
+                                                `
+                                                <div class="messageContainer">
+                                                    <div class="noteHeader">
+                                                        <h2>${message.title}</h2>
+
+                                                        <img onclick="deleteMessage('${message.id}')" src="./svg/delete.svg" alt="Deletar Mensagem">
+                                                    </div>
+
+                                                    <p>
+                                                        ${message.description}
+                                                    </p>
+                                                </div>
+                                                `
+
+                }else{
+                    console.log("Não encontrado")
+                }
+                
+
+            })
+           
+        })
+    })
+    .catch(err => console.log("Novo erro no search: " + err))
 }
 
 async function toggleAddMessage(){
@@ -83,4 +140,9 @@ function resetInputs(){
 
     title = " "
     description = ""
+}
+
+function newId() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1);
+    
 }
